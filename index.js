561bd09d96b6312e12003947a873a4278e93fc93
Yongho <dtaa0609@gmailcom>
2022-07-13 19:03:38 -0400

    Correct typo and add initial form of return app.post

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ var body_parser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res){
-  res.sendfile(__dirname + "/main.html");
+  res.sendFile(__dirname + "/main.html");
 });
 
 app.get("/rent.html", function(req, res){
-  res.sendfile(__dirname + "/rent.html");
+  res.sendFile(__dirname + "/rent.html");
 
 });
 
 app.get("/return.html", function(req, res){
-  res.sendfile(__dirname + "/return.html");
+  res.sendFile(__dirname + "/return.html");
 
 });
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
+});
+
+app.post("/return.html", function(res, req){
+  console.log(res);
+
+
 });
